36dcb9050f6d2d1de949e41d8064c726df6cf7ac
SirChanChan <64930470+chanchan69@users.noreply.github.com>
2022-03-14 20:20:05 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 @bot.event
 async def on_member_join(member):
     print(member.display_name + " was banned")
-    if [member.display_name == i for i in BLACKLIST]:
+    if member.display_name in BLACKLIST:
         await member.ban(reason="blacklisted username / automatic")
 
 bot.run(TOKEN)
